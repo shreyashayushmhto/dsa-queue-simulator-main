198a2a6bcc0f43dea7a42c7240f91ae5489993f4
Shreyash Mahato <mahatoshreyash@gmail.com>
2025-02-28 15:33:59 +0545

    some correctional changes
    correction

--- src/traffic_simulation.c
+++ src/traffic_simulation.c
@@ -6,14 +6,15 @@
 // Global queues for lanes
 // 0 = North, 1 = South, 2 = East, 3 = West
 // 0 = Normal, 1 = High priority
-Queue laneQueues[4];         // Queues for lanes A, B, C, D
-int lanePriorities[4] = {0}; // Priority levels for lanes (0 = normal, 1 = high)
+Queue laneQueues[4];  // Queues for lanes A, B, C, D, E, F
+int lanePriorities[4] = {0}; // Priori ty levels for lanes (0 = normal, 1 = high)
 
 const SDL_Color VEHICLE_COLORS[] = {
     {0, 0, 255, 255}, // REGULAR_CAR: Blue
     {255, 0, 0, 255}, // AMBULANCE: Red
     {0, 0, 128, 255}, // POLICE_CAR: Dark Blue
     {255, 69, 0, 255} // FIRE_TRUCK: Orange-Red
+
 };
 
 //vehicle addition
