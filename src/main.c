0a120ef70e895254572ca6560a0c496febd88da5
Shreyash Mahato <mahatoshreyash@gmail.com>
2025-02-28 16:26:41 +0545

    intializing window
    initializing  setup

--- src/main.c
+++ src/main.c
@@ -4,11 +4,11 @@
 #include "traffic_simulation.h"
 //file sharing established
 void initializeSDL(SDL_Window **window, SDL_Renderer **renderer) {
+    //initializing window and renderer
     SDL_Init(SDL_INIT_VIDEO);
     *window = SDL_CreateWindow("Traffic Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(*renderer, 173, 216, 230, 255); // Light blue
- 
+    SDL_SetRenderDrawColor(*renderer, 173, 216, 230, 255); // Light blue background
 }
 
 
